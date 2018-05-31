e9af3ff73ece5cbffb2aaa899e19903d9751be25
Symeon Symeonidis <simos421@gmail.com>
2018-05-31 19:46:50 +0300

    added Fdomain Solver

--- Simulation/CstDefineFrequencydomainSolver.m
+++ Simulation/CstDefineFrequencydomainSolver.m
@@ -1,16 +1,4 @@
-% Copyright (C) 2018  Symeon Symeonidis, Stefanos Tsantilas, Stelios Mitilineos
-% simos421@gmail.com, steftsantilas@gmail.com, smitil@gmail.com
-% This program is free software: you can redistribute it and/or modify
-% it under the terms of the GNU General Public License as published by
-% the Free Software Foundation, either version 3 of the License, or
-% (at your option) any later version.
-% 
-% This program is distributed in the hope that it will be useful,
-% but WITHOUT ANY WARRANTY; without even the implied warranty of
-% MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-% GNU General Public License for more details.
-
-function CstDefineFrequencydomainSolver(mws)
+function CstDefineFrequencydomainSolver(mws,startFreq,endFreq,samples)
 
 
 
@@ -23,12 +11,12 @@ Mesh = invoke(mws,'Mesh');
 invoke(Mesh,'SetCreator','High Frequency');
 FDSolver = invoke(mws,'FDSolver');
 invoke(FDSolver,'Reset');
-invoke(FDSolver,'SetMethod','Hexahedral','General purpose');
+invoke(FDSolver,'SetMethod','Tetrahedral','General purpose');
 invoke(FDSolver,'OrderTet','Second');
 invoke(FDSolver,'OrderSrf','First');
-invoke(FDSolver,'Stimulation','All','1');
+invoke(FDSolver,'Stimulation','All','All');
 invoke(FDSolver,'ResetExcitationList');
-invoke(FDSolver,'AutoNormImpedance','False');
+invoke(FDSolver,'AutoNormImpedance','True');
 invoke(FDSolver,'NormingImpedance','50');
 invoke(FDSolver,'ModesOnly','False');
 invoke(FDSolver,'ConsiderPortLossesTet','True');
@@ -36,12 +24,9 @@ invoke(FDSolver,'SetShieldAllPorts','False');
 invoke(FDSolver,'AccuracyHex','1e-6');
 invoke(FDSolver,'AccuracyTet','1e-4');
 invoke(FDSolver,'AccuracySrf','1e-3');
-
-
-
 invoke(FDSolver,'LimitIterations','False');
 invoke(FDSolver,'MaxIterations','0');
-invoke(FDSolver,'SetCalculateExcitationsInParallel','True','False','');
+invoke(FDSolver,'SetCalculateExcitationsInParallel','True','True','');
 invoke(FDSolver,'StoreAllResults','False');
 invoke(FDSolver,'StoreResultsInCache','False');
 invoke(FDSolver,'UseHelmholtzEquation','True');
@@ -67,35 +52,56 @@ invoke(FDSolver,'MixedOrderSrf','False');
 invoke(FDSolver,'MixedOrderTet','False');
 invoke(FDSolver,'PreconditionerAccuracyIntEq','0.15');
 invoke(FDSolver,'MLFMMAccuracy','Default');
-invoke(FDSolver,'MinMLFMMBoxSize','0.20');
+invoke(FDSolver,'MinMLFMMBoxSize','0.3');
 invoke(FDSolver,'UseCFIEForCPECIntEq','true');
 
 
 invoke(FDSolver,'UseFastRCSSweepIntEq','false');   
 invoke(FDSolver,'UseSensitivityAnalysis','False');
-invoke(FDSolver,'SweepErrorThreshold','True','0.01');
-invoke(FDSolver,'SweepErrorChecks','2');
+% invoke(FDSolver,'SweepErrorThreshold','True','0.01');
+invoke(FDSolver,'RemoveAllStopCriteria','Hex');
+invoke(FDSolver,'AddStopCriterion','All S-Parameters','0.01','2','Hex','True');
+invoke(FDSolver,'AddStopCriterion','Reflection S-Parameters','0.01','2','Hex','False');
+invoke(FDSolver,'AddStopCriterion','Transmission S-Parameters','0.01','2','Hex','False');
+invoke(FDSolver,'RemoveAllStopCriteria','Tet');
+
+invoke(FDSolver,'AddStopCriterion','All S-Parameters','0.01','2','Tet','True');
+invoke(FDSolver,'AddStopCriterion','Reflection S-Parameters','0.01','2','Tet','False');
+invoke(FDSolver,'AddStopCriterion','Transmission S-Parameters','0.01','2','Tet','False');
+% invoke(FDSolver,'All Probes','0.05','2','Tet','True');
+invoke(FDSolver,'RemoveAllStopCriteria','Srf');
+
+invoke(FDSolver,'AddStopCriterion','All S-Parameters','0.01','2','Srf','True');
+invoke(FDSolver,'AddStopCriterion','Reflection S-Parameters','0.01','2','Srf','False');
+invoke(FDSolver,'AddStopCriterion','Transmission S-Parameters','0.01','2','Srf','False');
 invoke(FDSolver,'SweepMinimumSamples','3');
-invoke(FDSolver,'SweepConsiderAll','True');
-invoke(FDSolver,'SweepConsiderReset');
 invoke(FDSolver,'SetNumberOfResultDataSamples','1001');
+
+invoke(FDSolver,'SetResultDataSamplingMode','Automatic');
 invoke(FDSolver,'SweepWeightEvanescent','1.0');
 invoke(FDSolver,'AccuracyROM','1e-4');
 invoke(FDSolver,'AddSampleInterval','','','1','Automatic','True');
-%invoke(FDSolver,'AddSampleInterval','','','','Automatic','False');
+invoke(FDSolver,'AddSampleInterval',num2str(startFreq),num2str(endFreq),num2str(samples),'Automatic','False');
+% invoke(FDSolver,'AddSampleInterval','','','','Automatic','False');
 invoke(FDSolver,'MPIParallelization','False');
 invoke(FDSolver,'UseDistributedComputing','False');
 invoke(FDSolver,'NetworkComputingStrategy','RunRemote');
 invoke(FDSolver,'NetworkComputingJobCount','3');
 invoke(FDSolver,'LimitCPUs','True');
-invoke(FDSolver,'MaxCPUs','48');
+invoke(FDSolver,'MaxCPUs','96');
+invoke(FDSolver,'MaximumNumberOfCPUDevices','2');
+
      
 IESolver = invoke(mws,'IESolver');
 invoke(IESolver,'Reset');    
 invoke(IESolver,'UseFastFrequencySweep','True'); 
 invoke(IESolver,'UseIEGroundPlane','False'); 
 % invoke(IESolver,'SetRealGroundMaterialName',''); 
+invoke(IESolver,'CalcFarFieldInRealGround','False'); 
+invoke(IESolver,'RealGroundModelType','Auto'); 
 invoke(IESolver,'PreconditionerType','Auto'); 
+invoke(IESolver,'ExtendThinWireModelByWireNubs','False');
+
 
 IESolver = invoke(mws,'IESolver');
 invoke(IESolver,'SetFMMFFCalcStopLevel','0');
@@ -113,8 +119,19 @@ invoke(IESolver,'SetOpenBC_XY','True');
 invoke(IESolver,'OldRCSSweepDefintion','False');
 invoke(IESolver,'SetAccuracySetting','Custom');
 invoke(IESolver,'CalculateSParaforFieldsources','True');
-Solver = invoke(mws,'Solver');
-invoke(Solver,'Start');
+
+invoke(IESolver,'ModeTrackingCMA','True');
+invoke(IESolver,'NumberOfModesCMA','3');
+invoke(IESolver,'StartFrequencyCMA','-1.0');
+invoke(IESolver,'SetAccuracySettingCMA','Default');
+invoke(IESolver,'FrequencySamplesCMA','0');
+invoke(IESolver,'SetMemSettingCMA','Auto');
+
+
+
+
+FDSolver = invoke(mws,'FDSolver');
+invoke(FDSolver,'Start');
       
 
 
